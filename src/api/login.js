5cc060678223c38b8b31f310314f32fc701cae5b
mimkong <shinhw12@naver.com>
2023-10-09 20:59:47 +0900

    req.body JSON파싱

--- src/api/login.js
+++ src/api/login.js
@@ -1,10 +1,26 @@
-module.exports = (req, res) => {
-  console.log(req.body);
+module.exports = async (req, res) => {
+  let parsedBody;
+
+  // Content-Type 헤더가 application/json인 경우
+  if (req.headers["content-type"] === "application/json") {
+    try {
+      parsedBody = await req.json();
+    } catch (err) {
+      res.status(400).send("Invalid JSON");
+      return;
+    }
+  } else {
+    res.status(400).send("Expected Content-Type: application/json");
+    return;
+  }
+
+  console.log(parsedBody);
+
   if (req.method !== "POST") {
     return res.status(405).end();
   }
 
-  const { userId, password } = req.body;
+  const { userId, password } = parsedBody;
 
   if (userId === "testId" && password === "testPassword1") {
     res.status(200).json({ token: "fake_token", message: "로그인 성공!" });
